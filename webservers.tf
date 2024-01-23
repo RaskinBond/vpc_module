82d95e217d64fdd6a8638945efd67c8f318e57b2
nykzaR <nykzar@outlook.com>
2024-01-23 14:57:06 +0530

    added changes

--- webservers.tf
+++ webservers.tf
@@ -8,10 +8,12 @@ resource "aws_instance" "WebServer" {
   ami                           = data.aws_ami.latest-amazon-linux-image.id
   instance_type                 = var.webserver_info.instance_type
   associate_public_ip_address   = var.webserver_info.public_ip_enabled
-  subnet_id                     = data.aws_subnets.subnets.ids[count.index]
+  subnet_id                     = lookup(aws_subnet.subnets[count.index].tags_all, "Name", "")
   vpc_security_group_ids        = [aws_security_group.Web-SG.id]
 
   tags                          = {
       Name                      = "${local.env_prefix}-server"
   }
+  depends_on           = [ aws_vpc.vnet, aws_subnet.subnets ]
+
 }
